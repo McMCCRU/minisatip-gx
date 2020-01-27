bad37a6896274166b46cfb4980b0c56f5efe4e79
McMCC <mcmcc@mail.ru>
2020-01-27 15:29:37 +0300

    add build/with libdvbcsa.

--- build.sh
+++ build.sh
@@ -2,9 +2,15 @@
 
 export PATH=/opt/goxceed/csky-linux-tools-i386-uclibc-20170724/bin:$PATH
 
+cd ./libdvbcsa
+./build.sh
+cd ..
+
 if [ ! -f config.log ]; then
-	./configure --host=csky-linux --enable-gxapi --enable-static --disable-satipc
+	CFLAGS="-I./libdvbcsa/src" LDFLAGS="-L./libdvbcsa/src/.libs" ./configure --host=csky-linux --enable-gxapi --enable-static --enable-dvbcsa --disable-satipc
 fi
 
+make clean
 make
+
 csky-linux-strip minisatip
