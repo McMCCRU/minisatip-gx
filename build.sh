3f07296118d8723a607b0db5929b95adf43a340f
McMCC <mcmcc@mail.ru>
2020-01-30 17:17:22 +0300

    updated build.sh scripts, adding in them comments for install toolchain...

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-export PATH=/opt/goxceed/csky-linux-tools-i386-uclibc-20170724/bin:$PATH
+# Install toolchain for C-SKY:
+# Ubuntu/Debian
+# sudo add-apt-repository ppa:nationalchip-repo/gxtools
+# sudo apt-get update
+# sudo apt-get install csky-linux-tools-uclibc-2.8.01
+# Manually:
+# wget https://launchpad.net/~nationalchip-repo/+archive/ubuntu/gxtools/+sourcefiles/csky-linux-tools-uclibc-2.8.01/1.0.0.0/csky-linux-tools-uclibc-2.8.01_1.0.0.0.tar.xz
+#
+# export PATH=/opt/gxtools/csky-linux-tools-uclibc-20170724/bin:$PATH
+
+export PATH=/opt/goxceed/csky-linux-tools-i386-uclibc-20170724/bin:/opt/gxtools/csky-linux-tools-uclibc-20170724/bin:$PATH
 
 cd ./libdvbcsa
 ./build.sh
--- libdvbcsa/build.sh
+++ libdvbcsa/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export PATH=/opt/goxceed/csky-linux-tools-i386-uclibc-20170724/bin:$PATH
+export PATH=/opt/goxceed/csky-linux-tools-i386-uclibc-20170724/bin:/opt/gxtools/csky-linux-tools-uclibc-20170724/bin:$PATH
 
 if [ ! -f config.log ]; then
 	./configure --host=csky-linux --enable-static --disable-shared
