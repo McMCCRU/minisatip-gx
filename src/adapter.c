2c8c7ad8f30383a33c132263d741013a76c710df
McMCC <mcmcc@mail.ru>
2020-02-01 00:59:53 +0300

    fix multiple threads work and set default use threads.

--- src/adapter.c
+++ src/adapter.c
@@ -2125,7 +2125,7 @@ int signal_thread(sockets *s)
 		if (ad->status_cnt++ <= 0) // make sure the kernel has updated the status
 			continue;
 		// do not get the signal when the adapter is being changed
-		if (ad->mutex.state != 0)
+		if (!opts.no_threads && ad->mutex.state != 0)
 			continue;
 		if (opts.no_threads && !ad->fast_status && status >= 0)
 			continue;
